6f73ef920c9afc23b92bafc42a09abf1b494c96c
unknown <mdyasirofficial2001@gmail.com>
2023-03-07 01:37:08 +0600

    Pagination added in collection section

--- src/Pages/Collections/Collections/Collections.js
+++ src/Pages/Collections/Collections/Collections.js
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "../../../Contexts/DataProvider/DataProvider";
+import Pagination from "../Pagination/Pagination";
 import SingleProduct from "../SingleProduct/SingleProduct";
 
 const Collections = () => {
   // All Bags Data from Data Contexts
   const { allBags } = useContext(DataContext);
-  console.log(allBags);
+  //   console.log(allBags);
+
+  //   Pagination Utils
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(10);
+
+  const lastProductIndex = currentPage * productsPerPage;
+  const firstProductIndex = lastProductIndex - productsPerPage;
+  const fixedBagsPerPage = allBags.slice(firstProductIndex, lastProductIndex);
   return (
     <div className="">
       <div className="flex justify-center">
@@ -16,10 +25,17 @@ const Collections = () => {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        {allBags.map((bag) => (
+        {fixedBagsPerPage.map((bag) => (
           <SingleProduct key={bag._id} bag={bag}></SingleProduct>
         ))}
       </div>
+      <Pagination
+        totalProducts={allBags.length}
+        productsPerPage={productsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        setProductsPerPage={setProductsPerPage}
+      ></Pagination>
     </div>
   );
 };
---
+++ src/Pages/Collections/Pagination/Pagination.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Pagination = ({
+  totalProducts,
+  productsPerPage,
+  setCurrentPage,
+  currentPage,
+}) => {
+  // Page Number setup
+  let pages = [];
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pages.push(i);
+  }
+  console.log(pages);
+  return (
+    <div>
+      <h1>
+        {pages.map((page, i) => (
+          <button
+            key={i}
+            page={page}
+            onClick={() => setCurrentPage(page)}
+            className={
+              page === currentPage
+                ? "active-page-number-style"
+                : "inactive-page-number-style"
+            }
+          >
+            {page}
+          </button>
+        ))}
+      </h1>
+    </div>
+  );
+};
+
+export default Pagination;
