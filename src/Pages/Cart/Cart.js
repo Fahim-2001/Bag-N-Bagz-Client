0dc746d9b47bed260d35cbeb4c8262eee6e6ab61
unknown <mdyasirofficial2001@gmail.com>
2023-03-12 00:25:58 +0600

    Displayed cart products in car.js

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -5,9 +5,14 @@ import CartProduct from "./CartProduct";
 const Cart = () => {
   const { cartProducts } = useContext(DataContext);
 
+  let totalPrice = 0;
+  for (const cartProduct of cartProducts) {
+    totalPrice = totalPrice + cartProduct.price * cartProduct.quantity;
+  }
   return (
     <div>
       <h1>Your Cart</h1>
+
       <div>
         {cartProducts.map((cartProduct) => (
           <CartProduct
--- src/Pages/Cart/CartProduct.js
+++ src/Pages/Cart/CartProduct.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const CartProduct = ({ cartProduct }) => {
-  console.log(cartProduct);
+  const { bag_name, img, price, quantity } = cartProduct;
   return (
-    <div>
-      <h1>{cartProduct.bag_name}</h1>
+    <div className="shadow-md mx-[2%] my-5 flex justify-between items-center">
+      <div className="flex items-center">
+        <h1 className="px-3">{bag_name}</h1>
+        <div className="mx-[5%] py-1">
+          <img className="h-20 " src={img} alt="" />
+        </div>
+      </div>
+      <div className="px-5">
+        <span>Quantity: {quantity}</span>
+      </div>
+      <div className="px-3">
+        <p>Price:{price * quantity}</p>
+      </div>
     </div>
   );
 };
