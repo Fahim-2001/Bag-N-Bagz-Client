b72052d10e2f24ff373e90d77f03a585616615b5
unknown <mdyasirofficial2001@gmail.com>
2023-03-21 21:11:06 +0600

    Latest

--- src/Pages/Checkout/CheckoutForm.js
+++ src/Pages/Checkout/CheckoutForm.js
@@ -1,25 +1,11 @@
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import {
+  CardElement,
+  CardNumberElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
 import React, { useState } from "react";
-
-const CARD_OPTIONS = {
-  iconStyle: "solid",
-  style: {
-    base: {
-      iconColor: "#c4f0ff",
-      color: "#fff",
-      fontWeight: 500,
-      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-      fontSize: "16px",
-      fontSmoothing: "antialiased",
-      ":-webkit-autofill": { color: "#fce883" },
-      "::placeholder": { color: "#87bbfd" },
-    },
-    invalid: {
-      iconColor: "#ffc7ee",
-      color: "#ffc7ee",
-    },
-  },
-};
 
 const CheckoutForm = () => {
   // Success state
@@ -34,46 +20,42 @@ const CheckoutForm = () => {
   //   Handlesubmit of form
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: "card",
-      elements: elements.getElement(CardElement),
+      card,
     });
 
-    if (!error) {
-      try {
-        const { id } = paymentMethod;
-        const response = await fetch("http://localhost:5000/payment", {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: {
-            ammount: 1000,
-            id,
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-          });
-        if (response.data.success) {
-          console.log("Payment Successful");
-          setSuccess(true);
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
+    if (error) {
+      console.log("[error]", error);
     } else {
-      console.log(error.message);
+      console.log("[PaymentMethod]", paymentMethod);
     }
   };
   return (
     <>
       {!success ? (
         <form action="" onSubmit={handleSubmit}>
-          <fieldset>
-            <div>
-              <CardElement options={CARD_OPTIONS}></CardElement>
-            </div>
-          </fieldset>
+          <div>
+            <p>Card Element</p>
+            <CardNumberElement></CardNumberElement>
+          </div>
+
           <button>Pay</button>
         </form>
       ) : (
